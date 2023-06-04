20fa9be4863e161d9786e28e2e9348ba147a7f06
alirezaJavaheri <alireza.jvh98@gmail.com>
2023-06-04 13:28:57 +0330

    chore: implement Header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+function Header() {
+  return (
+    <div className="w-100 p-fixed d-flex justify-center header-wrapper">
+      <div className="logo"></div>
+    </div>
+  );
+}
+
+export default Header;
