40517f980af70d3393d10a4f65962fb309043aa7
alirezaJavaheri <alireza.jvh98@gmail.com>
2023-06-04 11:53:51 +0330

    feat: create textField component

---
+++ src/components/TextField/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './style.scss';
+
+function TextField({ value, placeholder, onChange }) {
+  return (
+    <div className="wrapper">
+      <input
+        className="w-100"
+        value={value}
+        placeholder={placeholder}
+        onChange={({ target }) => onChange(target.value)}
+      />
+    </div>
+  );
+}
+
+TextField.propTypes = {
+  value: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default TextField;
